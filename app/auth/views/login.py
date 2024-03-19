69f6bf74820340b52d5a2bf73fc5f693e558ba5d
Dat <dat.lemindast@gmail.com>
2024-03-19 18:55:09 +0700

    change

--- app/auth/views/login.py
+++ app/auth/views/login.py
@@ -1,7 +1,8 @@
 from flask import (
     Flask,
     Blueprint,
-    render_template
+    render_template,
+    request
 )
 
 
@@ -9,4 +10,13 @@ auth = Blueprint("auth", __name__)
 
 @auth.route("/login", methods = ["POST", "GET"])
 def login():
+    if request.method == "POST":
+        if request.values() == "login":
+            return render_template("home.html")
     return render_template("login.html")
+
+
+@auth.route("/home",  methods = ["POST", "GET"])
+def home():
+    return render_template("home.html")
+    
