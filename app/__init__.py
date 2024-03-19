fd15eb34ce37bb11333f8b9dc0b649c81af303fa
Dat <dat.lemindast@gmail.com>
2024-03-19 10:59:23 +0700

    cahgne

---
+++ app/__init__.py
@@ -0,0 +1,59 @@
+from flask import (
+    Flask,
+    session,
+    url_for,
+    render_template,
+    Blueprint,
+    redirect,
+    request
+)
+import os
+
+def create_app(test_config=None):
+    """ app init
+
+    Args:
+        test_config (_type_, optional): _description_. Defaults to None.
+
+    Returns:
+        app : Flask app
+    """
+    app = Flask(__name__, instance_relative_config=True)
+    # create and configure the app
+    app.config.from_mapping(
+        SECRET_KEY='dev1911007',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        CACHE_TYPE = 'FileSystemCache',
+        CACHE_DIR = 'cache',
+        CACHE_THRESHOLD = 100000,
+    )
+    app.config.from_pyfile("config.py")
+    
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a base page
+    @app.route('/')
+    def main():
+        return redirect(url_for('auth_blueprint.login'))
+            
+    @app.route('/intro', methods = ['GET','POST'])
+    def intro():
+        if request.method == 'POST':
+            return redirect(url_for('auth_blueprint.index'))
+        return render_template("intro.html")
+    return app
+
+
+
+
