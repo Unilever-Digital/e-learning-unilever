27e93bc5183288d3367cf4c1a95604681a451a1c
Dat <dat.lemindast@gmail.com>
2024-03-22 19:51:04 +0700

    change

---
+++ app/static/js/home.js
@@ -0,0 +1,10 @@
+workspace = document.querySelector("#workspace");
+nav_bar = document.querySelector("#user-nav-bar");
+
+
+function eventClickDataPRocessing(){
+    const data = fetch("/data_getting",{
+        method = POST,
+        Content
+    }
+}
