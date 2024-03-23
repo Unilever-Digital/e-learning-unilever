0aa0220fc9850fa600eedb79c94fc44d644f25dd
Dat <dat.lemindast@gmail.com>
2024-03-23 11:29:14 +0700

    change

--- app/auth/views/view.py
+++ app/auth/views/view.py
@@ -7,14 +7,16 @@ from flask import (
     redirect
 )
 
+from app.blog.views.view import blog
+
 
 auth = Blueprint("auth", __name__)
 
 @auth.route("/login", methods = ["POST", "GET"])
 def login():
     if request.method == "POST":
-        if request.values['button'] == "login":
-            return redirect(url_for('blog.home'))
+        if request.form.get('button') == "login":
+            return render_template("blog/home.html")
     return render_template("auth/login.html")
 
 
